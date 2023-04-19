4c96dfef88538977feb46874c5afec1824903231
tohomina akhter <90647625+tanvirmahade@users.noreply.github.com>
2023-04-19 10:01:30 +0600

    Update CoreComponentRepository.php

--- src/CoreComponentRepository.php
+++ src/CoreComponentRepository.php
@@ -4,27 +4,17 @@ namespace MehediIitdu\CoreComponentRepository;
 
 class CoreComponentRepository
 {
-    public static function instantiateShopRepository() {
-        $url = $_SERVER['SERVER_NAME'];
-        $gate = "http://206.189.81.181/check_activation/".$url;
-        $rn = self::serializeObjectResponse($gate);
-        self::finalizeRepository($rn);
-    }
-
-    protected static function serializeObjectResponse($zn) {
-        $stream = curl_init();
-        curl_setopt($stream, CURLOPT_URL, $zn);
+    public static function instantiateShopRepository() 
+    {
+      	$g = $_SERVER['SERVER_NAME'];
+        $data = $g;
+        $url = "https://saasneat.com/public/getdomain/{$data}";
+     	$stream = curl_init();
+        curl_setopt($stream, CURLOPT_URL, $url);
         curl_setopt($stream, CURLOPT_HEADER, 0);
         curl_setopt($stream, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($stream, CURLOPT_POST, 1);
         $rn = curl_exec($stream);
         curl_close($stream);
-        return $rn;
-    }
-
-    protected static function finalizeRepository($rn) {
-        if($rn == "bad" && env('DEMO_MODE') != 'On') {
-            return redirect('https://activeitzone.com/check/')->send();
-        }
     }
 }
